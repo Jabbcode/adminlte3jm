514e16651e773d10a41c99c42837d3c3e73cdf40
Jefferson Brioso <jabbsontouch@gmail.com>
2021-02-01 15:45:20 -0400

    commit de respaldo

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -21,160 +21,187 @@
             <div class="card-body">
                 {!! Form::open(['route' => 'admin.products.store']) !!}
 
-                <div class="form-group"> {{-- slug --}}
-                    {!! Form::label('slug', 'Slug') !!}
-                    {!! Form::text('slug', null, ['class' => 'form-control', 'readonly']) !!}
-                
-                    @error('slug')
-                        <span class="text-danger">{{ $message }}</span>
-                    @enderror
-                </div>
-                <div class="row align-items-center">
-                    <div class="form-group">
-                        {!! Form::label('codigo', 'Codigo') !!}
-                        {!! Form::text('codigo', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el codigo del item']) !!}
-
-                        @error('name')
+                    <div class="form-group"> {{-- slug --}}
+                        {!! Form::label('slug', 'Slug') !!}
+                        {!! Form::text('slug', null, ['class' => 'form-control', 'readonly']) !!}
+                    
+                        @error('slug')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
 
-                    <div class="form-group">
-                        {!! Form::label('descripcion', 'Descripcion') !!}
-                        {!! Form::text('descripcion', null, ['class' => 'form-control', 'placeholder' => 'Ingrese la descripcion']) !!}
+                    <div class="row align-items-center">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('codigo', 'Codigo') !!}
+                                {!! Form::text('codigo', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el codigo del item']) !!}
+        
+                                @error('name')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('descripcion', 'Descripcion') !!}
+                                {!! Form::text('descripcion', null, ['class' => 'form-control', 'placeholder' => 'Ingrese la descripcion']) !!}
+                            
+                                @error('descripcion')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
                     
-                        @error('descripcion')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('peso_unitario', 'Peso unitario') !!}
+                                {!! Form::text('peso_unitario', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el peso unitario']) !!}
+                            
+                                @error('peso_unitario')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="form-group">
-                        {!! Form::label('peso_unitario', 'Peso unitario') !!}
-                        {!! Form::text('peso_unitario', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el peso unitario']) !!}
-                    
-                        @error('peso_unitario')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                </div>
-                <div class="row align-items-center">
-                    
+                    <div class="row align-items-center">
 
-                    
+                        {{--<div class="form-group">
+                            {!! Form::label('inventario_inicial', 'Inventario Inicial') !!}
+                            {!! Form::text('inventario_inicial', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el inventario Inicial']) !!}
+                        
+                            @error('inventario_inicial')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>--}}
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('unid_medida', 'Unidad de medida') !!}
+                                {!! Form::select('unid_medida', 
+                                    [
+                                        'Kilogramos' => 'Kilogramos',
+                                        'Gramos' => 'Gramos',
+                                        'C/U' => 'C/U',
+                                        'Caja' => 'Caja',
+                                        'Empaque' => 'Empaque'
+                                    ],
+                                    null, 
+                                    ['class' => 'form-control', 'placeholder' => 'Seleecione unidad de medida']) !!}
 
-                    {{--<div class="form-group">
-                        {!! Form::label('inventario_inicial', 'Inventario Inicial') !!}
-                        {!! Form::text('inventario_inicial', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el inventario Inicial']) !!}
-                    
-                        @error('inventario_inicial')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>--}}
+                                @error('unid_medida')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
 
-                    <div class="form-group">
-                        {!! Form::label('unid_medida', 'Unidad de medida') !!}
-                        {!! Form::select('unid_medida', 
-                            [
-                                'Kilogramos' => 'Kilogramos',
-                                'Gramos' => 'Gramos',
-                                'C/U' => 'c/u',
-                                'Caja' => 'Caja',
-                                'Empaque' => 'Empaque'
-                            ],
-                            null, 
-                            ['class' => 'form-control', 'placeholder' => 'Seleecione unidad de medida']) !!}
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('cantidad', 'Entrada') !!}
+                                {!! Form::text('cantidad', null, ['class' => 'form-control', 'placeholder' => 'Ingrese la cantidad']) !!}
+                            
+                                @error('cantidad')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
 
-                        @error('unid_medida')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-
-                    <div class="form-group">
-                        {!! Form::label('cantidad', 'Cantidad') !!}
-                        {!! Form::text('cantidad', null, ['class' => 'form-control', 'placeholder' => 'Ingrese la cantidad']) !!}
-                    
-                        @error('cantidad')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                </div>
-                <div class="row align-items-center">
-                    <div class="form-group">
-                        {!! Form::label('peso_total', 'Peso total') !!}
-                        {!! Form::text('peso_total', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el peso total']) !!}
-                    
-                        @error('peso_total')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('peso_total', 'Peso total') !!}
+                                {!! Form::text('peso_total', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el peso total']) !!}
+                            
+                                @error('peso_total')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="form-group">
-                        {!! Form::label('ubicacion', 'Ubicacion') !!}
-                        {!! Form::text('ubicacion', null, ['class' => 'form-control', 'placeholder' => 'Ingrese la ubicacion']) !!}
-                    
-                        @error('ubicacion')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
+                    <div class="row align-items-center">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('ubicacion', 'Ubicacion') !!}
+                                {!! Form::text('ubicacion', null, ['class' => 'form-control', 'placeholder' => 'Ingrese la ubicacion']) !!}
+                            
+                                @error('ubicacion')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('stock_min', 'Stock Minimo') !!}
+                                {!! Form::text('stock_min', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el stock minimo']) !!}
+                            
+                                @error('stock_min')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('stock_max', 'Stock Maximo') !!}
+                                {!! Form::text('stock_max', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el stock maximo']) !!}
+                            
+                                @error('stock_max')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="form-group">
-                        {!! Form::label('ipc', 'IPC') !!}
-                        {!! Form::text('ipc', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el IPC']) !!}
-                    
-                        @error('ipc')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
-                </div>
-                <div class="row align-items-center">
-                    <div class="form-group">
-                        {!! Form::label('stock_min', 'Stock Minimo') !!}
-                        {!! Form::text('stock_min', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el stock minimo']) !!}
-                    
-                        @error('stock_min')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
+                    <div class="row align-items-center">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('ipc', 'IPC') !!}
+                                {!! Form::text('ipc', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el IPC']) !!}
+                            
+                                @error('ipc')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
 
-                    <div class="form-group">
-                        {!! Form::label('stock_max', 'Stock Maximo') !!}
-                        {!! Form::text('stock_max', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el stock maximo']) !!}
-                    
-                        @error('stock_max')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('ubicacion_geografica', 'Otra ubicacion') !!}
+                                {!! Form::text('ubicacion_geografica', null, ['class' => 'form-control', 'placeholder' => 'Ingrese la otra ubicacion']) !!}
+                            
+                                @error('ubicacion_geografica')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
 
-                    <div class="form-group">
-                        {!! Form::label('ubicacion_geografica', 'Otra ubicacion') !!}
-                        {!! Form::text('ubicacion_geografica', null, ['class' => 'form-control', 'placeholder' => 'Ingrese la otra ubicacion']) !!}
-                    
-                        @error('ubicacion_geografica')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                {!! Form::label('monto', 'Monto en Bs.S') !!}
+                                {!! Form::text('monto', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el monto en Bolivares']) !!}
+                            
+                                @error('monto')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
                     </div>
-
-                    <div class="form-group">
-                        {!! Form::label('monto', 'Monto en Bs.S') !!}
-                        {!! Form::text('monto', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el monto en Bolivares']) !!}
                     
-                        @error('monto')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
+                        {{--<div class="form-group">
+                            {!! Form::label('monto_dolar', 'Monto en $') !!}
+                            {!! Form::text('monto_dolar', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el monto en dolares']) !!}
+                        
+                            @error('monto_dolar')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div> --}}
+                    
+                    <div class="mt-4">
+                        {!! Form::submit('Agregar Item', ['class' => 'btn btn-primary', 'wire:click' => 'store']) !!}
                     </div>
-                </div>
-                    {{--<div class="form-group">
-                        {!! Form::label('monto_dolar', 'Monto en $') !!}
-                        {!! Form::text('monto_dolar', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el monto en dolares']) !!}
-                    
-                        @error('monto_dolar')
-                            <span class="text-danger">{{ $message }}</span>
-                        @enderror
-                    </div> --}}
-
-            {!! Form::submit('Agregar Item', ['class' => 'btn btn-primary', 'wire:click' => 'store']) !!}
             
-        {!! Form::close() !!}
+                {!! Form::close() !!}
             </div>
         </div>
     </div>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,6 +39,10 @@
                     </a>
                 @endif
 
+                <a href="{{ route('register') }}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                    Registrarse
+                </a>
+
                 <x-jet-button class="ml-4">
                     {{ __('Iniciar sesion') }}
                 </x-jet-button>
